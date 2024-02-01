df7ada4c44f7eec07dc3474e74ddee1e74eeddec
Carlos Eduardo Culupú Aquino <eduardoculupu@gmail.com>
2024-01-31 19:51:23 -0500

    Consulta de  obtener un alquiler por Id usando Dapper

--- src/CleanArchitecture/CleanArchitecture.Application/Alquileres/GetAlquiler/GetAlquilerQueryHandler.cs
+++ src/CleanArchitecture/CleanArchitecture.Application/Alquileres/GetAlquiler/GetAlquilerQueryHandler.cs
@@ -1,6 +1,7 @@
 ﻿using CleanArchitecture.Application.Abstractions.Data;
 using CleanArchitecture.Application.Abstractions.Messaging;
 using CleanArchitecture.Domain.Abstractions;
+using Dapper;
 
 namespace CleanArchitecture.Application.Alquileres.GetAlquiler
 {
@@ -13,10 +14,39 @@ namespace CleanArchitecture.Application.Alquileres.GetAlquiler
             _sqlconnectionFactory = sqlconnectionFactory;
         }
 
-        public Task<Result<AlquilerResponse>> Handle(GetAlquilerQuery request, CancellationToken cancellationToken)
+        public async Task<Result<AlquilerResponse>> Handle(GetAlquilerQuery request, CancellationToken cancellationToken)
         {
+            using var connection = _sqlconnectionFactory.CreateConnection();
 
-            throw new NotImplementedException();
+            var sql = """
+                SELECT
+                    id AS Id
+                    , vehiculo_id AS VehiculoId
+                    , user_id AS UserId
+                    , status AS Status
+                    , precio_por_periodo AS PrecioAlquiler
+                    , precio_por_periodo_tipo_moneda AS TipoMonedaAlquiler
+                    , precio_mantenimiento AS PrecioMantenimiento
+                    , precio_mantenimiento_tipo_moneda AS TipoMonedaMantenimiento
+                    , precio_accesorios AS AccesoriosPrecio
+                    , precio_accesorios_tipo_moneda AS TipoMonedaAccesorio
+                    , precio_total AS PrecioTotal
+                    , precio_total_tipo_moneda AS TipoMonedaPrecioTotal
+                    , duracion_inicio AS DuracionInicio
+                    , duracion_final AS DuracionFinal
+                    , fecha_creacion AS FechaCreacion
+                FROM alquileres WHERE id = @AlquilerId
+            """;
+
+            var alquiler = await connection.QueryFirstOrDefaultAsync<AlquilerResponse>(
+                sql,
+                new
+                {
+                    request.AlquilerId
+                }
+            );
+
+            return alquiler!;
         }
     }
 }
